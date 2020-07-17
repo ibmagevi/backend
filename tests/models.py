3466c362d4781ee8d2961b2b3414fa0fc33a69ff
Paul Onteri <onteripaul@gmail.com>
2020-07-17 16:54:15 +0300

    Test model

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Test:
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, editable=False)
+    # True means positive
+    status = models.BooleanField(default=False)
+    hospital = models.CharField(max_length=30)
